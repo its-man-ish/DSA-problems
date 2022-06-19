f51cb199aa7830ca37499be003067b5ad3a10582
its-man-ish <manishbsn188@gmail.com>
2022-06-19 18:47:02 +0530

    prob7 better approch

--- array/prob7.cpp
+++ array/prob7.cpp
@@ -6,28 +6,24 @@ using namespace std;
 int removeDuplicate(int arr[], int n)
 {
     
-    int temp[n];
-    temp[0]=arr[0];
+   
     int res = 1;
     for (int i = 1; i < n; i++)
     {
-        if (temp[res-1]!=arr[i])
+        if (arr[res-1]!=arr[i])
         {
-            temp[res]=arr[i];
+            arr[res]=arr[i];
             res++;
         }
         
     }
-    for (int i = 0; i < res; i++)
-    {
-        arr[i]=temp[i];
-    }
+  
     
      cout<<"New array: ";
 
     for (int i = 0; i < res; i++)
     {
-        cout<<temp[i]<<" ";
+        cout<<arr[i]<<" ";
     }
     
     return 0;
